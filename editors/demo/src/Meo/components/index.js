41240aafd6b0d49165b68fbf4de58fe2a77a69fd
massifr <massifr@fastwebnet.it>
2021-04-22 11:34:39 +0200

    fix: purged code that is not needed

--- editors/demo/src/Meo/components/index.js
+++ editors/demo/src/Meo/components/index.js
@@ -1,3 +1,1 @@
 export { default as icons } from './icons/icons';
-export { default as CustomMenuButton } from './ui/CustomMenuButton';
-export { default as CustomIcon } from './helpers/CustomIcon';
